2519a38c30b927cca9161cf120593d7a0c703d61
KoreanGinseng <49602459+KoreanGinseng@users.noreply.github.com>
2020-05-25 14:58:39 +0900

    タイトルシーンの更新
    ゲームを始めるボタンを追加

--- Project/Title.cpp
+++ Project/Title.cpp
@@ -4,6 +4,12 @@ CTitle::CTitle(const CTitle::InitData & init) :
 	MyApp::CScene::IScene(init) 
 {
 	// 初期化処理
+
+	// ボタンフォントの登録。
+	m_pBtnFont = FontAsset("Button");
+
+	// スタートボタンの初期化。
+	m_BtnGameStart.Initialize(Vector2(350, 300), Vector2(100, 30), "始める", m_pBtnFont);
 }
 
 CTitle::~CTitle(void)
@@ -13,7 +19,7 @@ CTitle::~CTitle(void)
 
 void CTitle::Update(void)
 {
-	if (g_pInput->IsKeyPush(MOFKEY_RETURN))
+	if (g_pInput->IsKeyPush(MOFKEY_RETURN) || m_BtnGameStart.IsClickL())
 	{
 		ChangeScene(SceneName::Game);
 	}
@@ -23,4 +29,6 @@ void CTitle::Render(void) const
 {
 	CGraphicsUtilities::RenderString(0,  0, "TitleScene");
 	CGraphicsUtilities::RenderString(0, 30, "return : go GameScene");
+
+	m_BtnGameStart.Render();
 }
--- Project/Title.h
+++ Project/Title.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "Common.h"
+#include "Button.h"
 
 class CTitle : public MyApp::CScene
 {
 private:
+
+	std::shared_ptr<CFontA> m_pBtnFont;
+
+	CButton                 m_BtnGameStart;
 
 public:
 	
