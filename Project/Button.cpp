2b135c779010704c5c048947b799affed215cbf6
KoreanGinseng <49602459+KoreanGinseng@users.noreply.github.com>
2020-06-28 15:11:06 +0900

    ボタンクラスの追加

---
+++ Project/Button.cpp
@@ -0,0 +1,173 @@
+#include "Button.h"
+
+CButton::CButton(void) :
+	CButton(CRectangle())
+{
+}
+
+CButton::CButton(const CButton & btn)
+{
+	m_Rect        = btn.m_Rect;
+	m_Color[0]    = btn.m_Color[0];
+	m_Color[1]    = btn.m_Color[1];
+	m_Color[2]    = btn.m_Color[2];
+	m_Color[3]    = btn.m_Color[3];
+	m_FontColor   = btn.m_FontColor;
+	m_bMultiColor = btn.m_bMultiColor;
+	m_pFont       = btn.m_pFont;
+	m_pTexture    = btn.m_pTexture;
+	m_String      = btn.m_String;
+}
+
+CButton::CButton(const CRectangle& rect) :
+	CButton(rect, "", nullptr)
+{
+}
+
+CButton::CButton(const CRectangle& rect, const std::string & str, const std::shared_ptr<CFontA>& pFont) :
+	m_Rect(rect),
+	m_String(str),
+	m_pFont(pFont),
+	m_pTexture(nullptr),
+	m_bMultiColor(false),
+	m_FontColor(MOF_COLOR_BLACK)
+{
+	m_Color[0] = MOF_COLOR_WHITE;
+}
+
+CButton::~CButton(void)
+{
+	m_pFont = nullptr;
+	m_pTexture = nullptr;
+}
+
+void CButton::Render(void) const
+{
+	// テクスチャが設定されている場合とそうでない場合で描画方法を変える。
+	if (m_pTexture != nullptr)
+	{
+		RenderTexture();
+	}
+	else
+	{
+		RenderColor();
+	}
+
+	RenderFont();
+}
+
+void CButton::RenderTexture(void) const
+{
+	m_pTexture->Render(m_Rect);
+}
+
+void CButton::RenderColor(void) const
+{
+	if (m_bMultiColor)
+	{
+		CGraphicsUtilities::RenderFillRect(m_Rect, m_Color[0], m_Color[1], m_Color[2], m_Color[3]);
+	}
+	else
+	{
+		CGraphicsUtilities::RenderFillRect(m_Rect, m_Color[0]);
+	}
+}
+
+void CButton::RenderFont(void) const
+{
+	CRectangle outRect;
+
+	// フォントが設定されているかどうかで処理を変える。
+	// 中央寄せで文字を描画する。
+	if (m_pFont != nullptr)
+	{
+		m_pFont->CalculateStringRect(0, 0, m_String.c_str(), outRect);
+
+		float x = (m_Rect.GetWidth()  - outRect.GetWidth() ) * 0.5f;
+		float y = (m_Rect.GetHeight() - outRect.GetHeight()) * 0.5f;
+
+		m_pFont->RenderString(m_Rect.Left + x, m_Rect.Top + y, m_FontColor, m_String.c_str());
+	}
+	else
+	{
+		CGraphicsUtilities::CalculateStringRect(0, 0, m_String.c_str(), outRect);
+
+		float x = (m_Rect.GetWidth()  - outRect.GetWidth() ) * 0.5f;
+		float y = (m_Rect.GetHeight() - outRect.GetHeight()) * 0.5f;
+
+		CGraphicsUtilities::RenderString(m_Rect.Left + x, m_Rect.Top + y, m_FontColor, m_String.c_str());
+	}
+}
+
+void CButton::SetString(const std::string & str)
+{
+	m_String = str;
+}
+
+void CButton::SetFont(const std::shared_ptr<CFontA>& pFont)
+{
+	m_pFont = pFont;
+}
+
+void CButton::SetTexture(const std::shared_ptr<CTexture>& pTexture)
+{
+	m_pTexture = pTexture;
+}
+
+void CButton::SetFontColor(const MofU32 & color)
+{
+	m_FontColor = color;
+}
+
+void CButton::SetColor(const MofU32 & color)
+{
+	m_bMultiColor = false;
+	m_Color[0] = color;
+}
+
+void CButton::SetColor(const MofU32 & ltc, const MofU32 & lbc, const MofU32 & rtc, const MofU32 & rbc)
+{
+	m_bMultiColor = true;
+	m_Color[0] = ltc;
+	m_Color[1] = lbc;
+	m_Color[2] = rtc;
+	m_Color[3] = rbc;
+}
+
+bool CButton::IsPush(void) const
+{
+	Vector2 mousePos;
+	g_pInput->GetMousePos(mousePos);
+	if (GetRect().CollisionPoint(mousePos) && g_pInput->IsMouseKeyPush(MOFMOUSE_LBUTTON))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool CButton::IsHold(void) const
+{
+	Vector2 mousePos;
+	g_pInput->GetMousePos(mousePos);
+	if (GetRect().CollisionPoint(mousePos) && g_pInput->IsMouseKeyHold(MOFMOUSE_LBUTTON))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool CButton::IsPull(void) const
+{
+	Vector2 mousePos;
+	g_pInput->GetMousePos(mousePos);
+	if (GetRect().CollisionPoint(mousePos) && g_pInput->IsMouseKeyPull(MOFMOUSE_LBUTTON))
+	{
+		return true;
+	}
+	return false;
+}
+
+CRectangle CButton::GetRect(void) const
+{
+	return m_Rect;
+}
---
+++ Project/Button.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "Common.h"
+
+class CButton
+{
+private:
+
+	CRectangle                m_Rect;
+
+	MofU32                    m_Color[4];
+
+	MofU32                    m_FontColor;
+
+	bool                      m_bMultiColor;
+
+	std::shared_ptr<CFontA>   m_pFont;
+
+	std::shared_ptr<CTexture> m_pTexture;
+
+	std::string               m_String;
+
+	void RenderTexture(void) const;
+
+	void RenderColor(void) const;
+
+	void RenderFont(void) const;
+
+public:
+
+	CButton(void);
+
+	CButton(const CButton& btn);
+
+	CButton(const CRectangle& rect);
+	
+	CButton(const CRectangle& rect, const std::string& str, const std::shared_ptr<CFontA>& pFont = nullptr);
+
+	~CButton(void);
+
+	void Render(void) const;
+
+	void SetString(const std::string& str);
+
+	void SetFont(const std::shared_ptr<CFontA>& pFont);	
+
+	void SetTexture(const std::shared_ptr<CTexture>& pTexture);
+
+	void SetFontColor(const MofU32& color);
+
+	void SetColor(const MofU32& color);
+
+	void SetColor(const MofU32& ltc, const MofU32& lbc, const MofU32& rtc, const MofU32& rbc);
+
+	bool IsPush(void) const;
+
+	bool IsHold(void) const;
+
+	bool IsPull(void) const;
+
+	CRectangle GetRect(void) const;
+};
+
