48d6f741f01851027e1f5d7d6867cfc0c21f497d
KoreanGinseng <49602459+KoreanGinseng@users.noreply.github.com>
2020-07-27 17:26:21 +0900

    読み込み画面の作成

--- Project/Load.cpp
+++ Project/Load.cpp
@@ -1,21 +1,38 @@
 #include "Load.h"
+#include "Setting.h"
 
 CLoad::CLoad(const CLoad::InitData & init) :
 	CLoadBase(init)
 {
 	// 初期化処理
+	m_pFont = FontAsset("Info");
+	m_pFont->SetSize(60);
+
+	m_AlphaMotion << CEaseMotion<int>(255,   0, Ease::In, EaseType::None, 0.5f);
+	m_AlphaMotion << CEaseMotion<int>(  0, 255, Ease::In, EaseType::None, 0.5f);
+
+	m_AlphaMotion.Start();
 }
 
 CLoad::~CLoad(void)
 {
 	// 解放処理
-
+	GetData().keyLength = CSetting::GetKeyLength();
+	GetData().offsetKey = CSetting::GetKeyOffset();
 }
 
 void CLoad::Update(void)
 {
 	// 読み込み中の更新処理。
+	m_BackRender.Update();
 
+	if (m_AlphaMotion.IsEnd())
+	{
+		m_AlphaMotion.Reset();
+		m_AlphaMotion.Start();
+	}
+
+	m_AlphaMotion.Update();
 
 	// スレッドでの読み込み制御。
 	CLoadBase::Update();
@@ -25,7 +42,10 @@ void CLoad::Render(void) const
 {
 	g_pGraphics->ClearTarget(0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0);
 
-	CGraphicsUtilities::RenderString(0, 0, "Loading");
+	CRectangle outRect;
+	m_pFont->CalculateStringRect(0, 0, "Loading", outRect);
+	m_pFont->RenderString((SceneWidth - outRect.GetWidth()) * 0.5f, SceneHeight * 0.5f + 100, MOF_ALPHA_WHITE(m_AlphaMotion.GetValue()), "Loading");
 
-	// TODO : Loading中のアニメーション描画。
+	// Loading中のアニメーション描画。
+	m_BackRender.Render();
 }
--- Project/Load.h
+++ Project/Load.h
@@ -1,8 +1,17 @@
 #pragma once
 #include "LoadBase.h"
+#include "BackRender.h"
 
 class CLoad : public CLoadBase
 {
+private:
+
+	CBackRender                m_BackRender;
+
+	CEaseMotionController<int> m_AlphaMotion;
+
+	std::shared_ptr<CFontA>    m_pFont;
+
 public:
 
 	CLoad(const CLoad::InitData& init);
